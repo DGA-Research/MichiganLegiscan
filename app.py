a6cc948f0eb2651fd9f8e6e88d40b40f4b9431cf
DGA-Research <dgaresearchcoding@gmail.com>
2025-07-01 13:51:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ elif sponsor_button:
         # find people_id
         people_id = getPeopleID(selected_name)
         activeSessions = getSessions(people_id)
-        sponsoredBills = getSponsorships(peopleID, activeSessions)
+        sponsoredBills = getSponsorships(people_ID, activeSessions)
         data = []
         for bill_id, values in sponsoredBills.items():
             row = {
