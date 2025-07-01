d8759425d8770e85bf551b50e99c05602aa3a9f0
DGA-Research <dgaresearchcoding@gmail.com>
2025-07-01 13:46:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -72,6 +72,24 @@ def getVotes(peopleID, activeSessions):
     return(roll_call_dict)
 
 
+def getSponsorships(peopleID, activeSessions):
+    # get list of all sponsored bill id
+    sponsored_bill_ids = []
+    for session in activeSessions:
+        with open(f"{session}/sponsors.csv", "r", newline='') as file_in:
+            reader = csv.DictReader(file_in)
+            for row in reader:
+                if row["people_id"] == peopleID:
+                    sponsored_bill_ids.append(row["bill_id"])
+    sponsored_bill_ids_dict = {}
+    # Using bill_id, get bill info from bills.csv
+    for session in activeSessions:
+        with open(f"{session}/bills.csv", "r", newline='') as file_in:
+            reader = csv.DictReader(file_in)
+            for row in reader:
+                if row["bill_id"] in sponsored_bill_ids:
+                    sponsored_bill_ids_dict[row["bill_id"]] = [row["bill_number"], row["status_date"], row["status_desc"], row["title"], row["description"], row["url"]]
+    return sponsored_bill_ids_dict
 
 names = getListNames()
 
@@ -84,6 +102,7 @@ selected_name = st.selectbox(
 st.write(f"You selected: {selected_name}")
 
 votes_button = st.button(f"Get {selected_name}'s voting record")
+sponsor_button = st.button(f"Get {selected_name}'s sponsored bill record")
 
 if votes_button:
     with st.spinner("Pulling voting records..."):
@@ -95,12 +114,36 @@ if votes_button:
         for roll_call_id, values in votingRecord.items():
             row = {
             'Session': values[1],
-            'Date': values[4],
-            'Bill Number': values[3],
+            'Date': values[9],
+            'Bill Number': values[8],
             'Vote': values[0].strip(),  # remove \n
-            'Bill Description': values[5],
+            'Bill Description': values[10],
+            'Yea': values[3],
+            'Nay': values[4],
+            'No Vote: values[5],
+            'Absent: values[6],
             'Bill ID': values[2],
             'Roll Call ID': roll_call_id
+            }
+            data.append(row)
+        df = pd.DataFrame(data)
+        st.write(df)
+
+elif sponsor_button:
+     with st.spinner("Pulling sponsored bills..."):
+        # find people_id
+        people_id = getPeopleID(selected_name)
+        activeSessions = getSessions(people_id)
+        sponsoredBills = getSponsorships(peopleID, activeSessions)
+        data = []
+        for bill_id, values in sponsoredBills.items():
+            row = {
+            'Date': values[1],
+            'Bill Number': values[0],
+            'Status': values[2]
+            'Bill Description': values[3],
+            'URL': values[5],
+            'Bill ID': bill_id
             }
             data.append(row)
         df = pd.DataFrame(data)
